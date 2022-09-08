a1f49c4955252f1d7d536ef5d106562173707d8f
Victor Leiderö-Palmlöv <leideropalmlov.victor@elev.ga.ntig.se>
2022-09-08 11:08:51 +0200

    Made the game logic but its buggy af

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,13 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using Raylib_cs;
 
 Console.WriteLine("Welcome your Tamagotchi is waiting for you!");
+Console.WriteLine("");
+Console.WriteLine("PRESS ENTER TO START");
 Console.ReadLine();
 
 Tamagotchi newTG = new Tamagotchi();
 Console.Clear();
 Console.WriteLine("What is your Tamagotchi's name going to be?");
 newTG.name = Console.ReadLine();
+
+
+while (newTG.GetAlive() == true)
+{
+    newTG.choice();
+
+    string userChoice = Console.ReadLine();
+    if (userChoice.ToLower() == "f")
+    {
+        newTG.feed();
+    }
+    if (userChoice.ToLower() == "p")
+    {
+        newTG.printStats();
+    }
+    if (userChoice.ToLower() == "h")
+    {
+        newTG.Hi();
+    }
+    if (userChoice.ToLower() == "t")
+    {
+        newTG.teach();
+    }
+    else
+    {
+        newTG.nothing();
+    }
+
+}
+
+Console.Clear();
+Console.WriteLine($"{newTG.name} just died");
+Console.WriteLine($"How are you so careless!");
+Console.WriteLine($"");
+Console.WriteLine($"Press ENTER to QUIT");
+Console.ReadLine();
 
 
 
--- Tamagotchi/tamagotchi.cs
+++ Tamagotchi/tamagotchi.cs
@@ -4,32 +4,38 @@ using System.Collections.Generic;
 public class Tamagotchi
 {
     public string name;
-
+    public string word;
     private int boredom;
     private int hunger;
-    
     private bool isAlive;
-
-
     private Random generator;
-
-    private List<string> words = new List<string>() {"Hello!"};
+    private List<string> words = new List<string>();
 
 public Tamagotchi()
 {
     isAlive = true;
-    generator = new Random();
+    hunger = 50;
+    boredom = 50;
+    // BASE STATS   
+
 }
 
 public void feed()
 {
-    Console.WriteLine($"{name} Tamagotchi just ate!");
+    int rndFeed = generator.Next(5,15);
+
+    Console.WriteLine($"{name} Tamagotchi just ate! +{rndFeed} hunger!");
 
 
-    hunger = hunger + 10;
+
+    hunger = hunger + rndFeed;
 
     if(hunger < 0) {hunger = 0; }
     if(hunger > 100){hunger = 100;}
+
+    Console.ReadLine();
+
+    choice();
     
      // jag tycker det är mer logiskt att hunger går ner och inte upp
 
@@ -38,21 +44,44 @@ public void feed()
 public void printStats()
 {
     Console.WriteLine($"Name: {name}, Hunger: {hunger}/100, Boredom: {boredom}");
+    Console.ReadLine();
+
+    choice();
 
 }
 
 private void reduceBoredom()
 {
-    Console.WriteLine($"{name} is having fun and is now less bored!");
-    boredom = boredom + 10;
+    int rndFun = generator.Next(5,15);
+
+    Console.WriteLine($"{name} is having fun and is now less bored! +{rndFun} boredom!");
+
+
+    boredom = boredom + rndFun;
+
     if(boredom > 0){boredom = 0;}
     if(boredom < 100){boredom = 100;}
+    Console.ReadLine();
+
+    choice();
 }
 
 public void Hi() 
 {
-    Console.WriteLine($"Hello, {words}!");
+    int rndWord = generator.Next(words.Count);
+
+    if(rndWord > 1 || rndWord == 1)
+    {
+    Console.WriteLine($"{name} says {words[rndWord]}!");
     reduceBoredom();
+    }
+    else if(rndWord == 0)
+    {
+        Console.WriteLine($"Teach {name} a word first!");
+    }
+    Console.ReadLine();
+    choice();
+
 }
 
 public void tick()
@@ -60,18 +89,49 @@ public void tick()
     boredom = boredom - 5;
     hunger = hunger - 5;
 
-    if(hunger < 10 || boredom < 10)
+    if(hunger < 0 || boredom < 0)
     {
         isAlive = false;
     }
 }
 
-public void teach(string word)
+public void teach()
 {
     Console.WriteLine($"What word would you like to teach {name}?");
     word = Console.ReadLine();
     words.Add(word);
     reduceBoredom();
     Console.WriteLine($"{name} just learnt the word: {word}!");
+    Console.ReadLine();
+
+    choice();
 }
+
+ public bool GetAlive()
+  {
+    return isAlive;
+  }
+
+  public void nothing()
+  {
+    Console.WriteLine("You're boring! Choose something next time!");
+    Console.ReadLine();
+
+    choice();
+  }
+
+  public void choice()
+  {
+    Console.Clear();
+    Console.WriteLine($"Choose one option:");
+    Console.WriteLine($"F: Dinner Time for {name}");
+    Console.WriteLine($"T: Teach {name} a word");
+    Console.WriteLine($"H: Say Hi to {name}");
+    Console.WriteLine($"P: See {name}'s Stats");
+    Console.WriteLine($"N: Do nothing");
+
+    tick();
+
+  }
+
 }
